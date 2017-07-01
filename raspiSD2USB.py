f33d4159ba050fa353ad362d2d4e4d59d30a0922
framp <framp@linux-tips-and-tricks.de>
2017-07-01 14:57:55 +0200

    Updated copyright year

--- raspiSD2USB.py
+++ raspiSD2USB.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#    Copyright (C) 2015-2016 framp at linux-tips-and-tricks dot de
+#    Copyright (C) 2015-2017 framp at linux-tips-and-tricks dot de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
